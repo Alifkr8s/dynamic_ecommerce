fdb3f186d577ac94eca65db947182ee1025cffb5
apurbosaha-maker <apurbo.saha@g.bracu.ac.bd>
2026-04-13 07:57:00 +0600

    updated profile request validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -3,24 +3,12 @@
 namespace App\Http\Requests;
 
 use App\Models\User;
-<<<<<<< HEAD
-=======
 use Illuminate\Contracts\Validation\ValidationRule;
->>>>>>> 53ee8e9e6af63cef39947ec0d1f997481c465bc0
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-<<<<<<< HEAD
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-=======
-     * @return array<string, ValidationRule|array<mixed>|string>
->>>>>>> 53ee8e9e6af63cef39947ec0d1f997481c465bc0
-     */
     public function rules(): array
     {
         return [
