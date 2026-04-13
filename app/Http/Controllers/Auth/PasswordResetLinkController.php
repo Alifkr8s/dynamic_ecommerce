4c11ae2cefcd95a9251197e8f3b28ea427e5e90b
apurbosaha-maker <apurbo.saha@g.bracu.ac.bd>
2026-04-13 08:38:40 +0600

    fixed merge conflict in password reset controller

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -6,10 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-<<<<<<< HEAD
-=======
 use Illuminate\Validation\ValidationException;
->>>>>>> 53ee8e9e6af63cef39947ec0d1f997481c465bc0
 use Illuminate\View\View;
 
 class PasswordResetLinkController extends Controller
@@ -25,11 +22,7 @@ class PasswordResetLinkController extends Controller
     /**
      * Handle an incoming password reset link request.
      *
-<<<<<<< HEAD
-     * @throws \Illuminate\Validation\ValidationException
-=======
      * @throws ValidationException
->>>>>>> 53ee8e9e6af63cef39947ec0d1f997481c465bc0
      */
     public function store(Request $request): RedirectResponse
     {
@@ -37,9 +30,6 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
             $request->only('email')
         );
